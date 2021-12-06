3837a8b7931dd094bfc8ade11076f91df8bf51fa
oilyshelf <Rost-Iskan@web.de>
2021-12-06 16:47:12 +0100

    test

--- src/test/test.py
+++ src/test/test.py
@@ -1,3 +1,10 @@
 from nodes.node import Node
 
 
+def test_nodes():
+    """dummy test for setup """
+    node1:Node = Node(1, "child")
+    node2:Node = Node(2, "parent", [1])
+    assert node1.name == "child"
+    assert node2.name == "parent"
+    assert node1 is not node2
