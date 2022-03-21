e174c1dabf34ba8b696ba6ad8f5dec9b004190d2
oilyshelf <oilyshelf@gmail.com>
2022-03-21 22:33:47 +0100

    added xmltodict

--- src/BPMN/BPMN_Parser.py
+++ src/BPMN/BPMN_Parser.py
@@ -1,3 +1,9 @@
+import xmltodict
+import json
+
 
 class BPMNParser():
-    pass
+    def load(self, bpmn_filepath):
+        final_definition = {}
+        json_process_definition = xmltodict.parse(bpmn_filepath)
+        return json.loads(json.dumps(json_process_definition.get("bpmn:definitions", {}).get("bpmn:process", {})))
