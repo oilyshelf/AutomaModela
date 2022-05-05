ce04433a5febcf0b46bd8cbf9fe2ee0d0fc10e64
oilyshelf (Rosti) <rotislav.iskandirov@vodafone.com>
2022-05-05 18:23:26 +0200

    fixed issue with concat

--- Notebooks/inkltest_bpmn.py
+++ Notebooks/inkltest_bpmn.py
@@ -3,66 +3,73 @@ import numpy as np
 #to run this code u need python 3.x with these packages installed pandas, numpy, numexpr
 
 #here we read the data provided by the beispiel.xlsx from the sheet Tabelle1 and set the index to None
-Token_KuPVLm14k_ytHoxfapD27A = pd.read_excel("beispiel.xlsx", sheet_name = 'Tabelle1')
+Token_fus_1Y8fARH3RHy09ihsXA = pd.read_excel("beispiel.xlsx", sheet_name = 'Tabelle1')
 
 #Here we try to evaluate the Expression Kostenstelle = "unknown" with diffrent engines
 for engine in [{}, {'engine': 'python'}]:
     try:
-        Token_KuPVLm14k_ytHoxfapD27A = Token_KuPVLm14k_ytHoxfapD27A.eval('Kostenstelle = "unknown"', **engine)
+        Token_fus_1Y8fARH3RHy09ihsXA = Token_fus_1Y8fARH3RHy09ihsXA.eval('Kostenstelle = "unknown"', **engine)
     except Exception:
         print(engine, "failed to evaluate", 'Kostenstelle = "unknown"', "trying next")
 
-#Creating new Dataframe based on Token_KuPVLm14k_ytHoxfapD27A
-Token_vjluOJwPVwjQM8G_OegF_w = Token_KuPVLm14k_ytHoxfapD27A.copy(True)
+#Creating new Dataframe based on Token_fus_1Y8fARH3RHy09ihsXA
+Token_9Q3svPWZmhCzfH4OwWMPEw = Token_fus_1Y8fARH3RHy09ihsXA.copy(True)
 
 #Here we try to query the Expression Profitcenter == "P1" with diffrent engines
 for engine in [{'engine': 'numexpr'}, {'engine': 'python'}]:
     try:
-        Token_vjluOJwPVwjQM8G_OegF_w = Token_vjluOJwPVwjQM8G_OegF_w.query('Profitcenter == "P1"', **engine)
+        Token_9Q3svPWZmhCzfH4OwWMPEw = Token_9Q3svPWZmhCzfH4OwWMPEw.query('Profitcenter == "P1"', **engine)
     except Exception:
         print(engine, "failed to query", 'Profitcenter == "P1"', "trying next")
 
-#Creating new Dataframe based on Token_KuPVLm14k_ytHoxfapD27A
-Token_LydPKuhaHg6VGJI09Hy_pw = Token_KuPVLm14k_ytHoxfapD27A.copy(True)
+#Creating new Dataframe based on Token_fus_1Y8fARH3RHy09ihsXA
+Token_zYchfRj78Yj1GOnTtn0tIA = Token_fus_1Y8fARH3RHy09ihsXA.copy(True)
 
 #Here we try to query the Expression Profitcenter == "P2" with diffrent engines
 for engine in [{'engine': 'numexpr'}, {'engine': 'python'}]:
     try:
-        Token_LydPKuhaHg6VGJI09Hy_pw = Token_LydPKuhaHg6VGJI09Hy_pw.query('Profitcenter == "P2"', **engine)
+        Token_zYchfRj78Yj1GOnTtn0tIA = Token_zYchfRj78Yj1GOnTtn0tIA.query('Profitcenter == "P2"', **engine)
     except Exception:
         print(engine, "failed to query", 'Profitcenter == "P2"', "trying next")
 
-#Creating new Dataframe based on Token_KuPVLm14k_ytHoxfapD27A
-Token_hgyulBaIApjsQZyA8qQ0yQ = Token_KuPVLm14k_ytHoxfapD27A.copy(True)
+#Creating new Dataframe based on Token_fus_1Y8fARH3RHy09ihsXA
+Token_vBu912o4OI4zQhWoDmj75g = Token_fus_1Y8fARH3RHy09ihsXA.copy(True)
 
 #Here we try to query the Expression ~(Profitcenter == "P1") & ~(Profitcenter == "P2") with diffrent engines
 for engine in [{'engine': 'numexpr'}, {'engine': 'python'}]:
     try:
-        Token_hgyulBaIApjsQZyA8qQ0yQ = Token_hgyulBaIApjsQZyA8qQ0yQ.query('~(Profitcenter == "P1") & ~(Profitcenter == "P2")', **engine)
+        Token_vBu912o4OI4zQhWoDmj75g = Token_vBu912o4OI4zQhWoDmj75g.query('~(Profitcenter == "P1") & ~(Profitcenter == "P2")', **engine)
     except Exception:
         print(engine, "failed to query", '~(Profitcenter == "P1") & ~(Profitcenter == "P2")', "trying next")
 
 #Here we try to evaluate the Expression Kostenstelle = "K2" with diffrent engines
 for engine in [{}, {'engine': 'python'}]:
     try:
-        Token_vjluOJwPVwjQM8G_OegF_w = Token_vjluOJwPVwjQM8G_OegF_w.eval('Kostenstelle = "K2"', **engine)
+        Token_9Q3svPWZmhCzfH4OwWMPEw = Token_9Q3svPWZmhCzfH4OwWMPEw.eval('Kostenstelle = "K2"', **engine)
     except Exception:
         print(engine, "failed to evaluate", 'Kostenstelle = "K2"', "trying next")
 
 #Here we try to evaluate the Expression Kostenstelle = "K1" with diffrent engines
 for engine in [{}, {'engine': 'python'}]:
     try:
-        Token_LydPKuhaHg6VGJI09Hy_pw = Token_LydPKuhaHg6VGJI09Hy_pw.eval('Kostenstelle = "K1"', **engine)
+        Token_zYchfRj78Yj1GOnTtn0tIA = Token_zYchfRj78Yj1GOnTtn0tIA.eval('Kostenstelle = "K1"', **engine)
     except Exception:
         print(engine, "failed to evaluate", 'Kostenstelle = "K1"', "trying next")
 
 #Here we concate two Dataframes
-Token_hgyulBaIApjsQZyA8qQ0yQ = pd.concat([Token_hgyulBaIApjsQZyA8qQ0yQ,Token_vjluOJwPVwjQM8G_OegF_w])
+Token_vBu912o4OI4zQhWoDmj75g = pd.concat([Token_vBu912o4OI4zQhWoDmj75g,Token_9Q3svPWZmhCzfH4OwWMPEw]).drop_duplicates(keep=True)
 
 #Here we concate two Dataframes
-Token_hgyulBaIApjsQZyA8qQ0yQ = pd.concat([Token_hgyulBaIApjsQZyA8qQ0yQ,Token_LydPKuhaHg6VGJI09Hy_pw])
+Token_vBu912o4OI4zQhWoDmj75g = pd.concat([Token_vBu912o4OI4zQhWoDmj75g,Token_zYchfRj78Yj1GOnTtn0tIA]).drop_duplicates(keep=True)
+
+#Here we try to evaluate the Expression Demo = "läuft gut" with diffrent engines
+for engine in [{}, {'engine': 'python'}]:
+    try:
+        Token_vBu912o4OI4zQhWoDmj75g = Token_vBu912o4OI4zQhWoDmj75g.eval('Demo = "läuft gut"', **engine)
+    except Exception:
+        print(engine, "failed to evaluate", 'Demo = "läuft gut"', "trying next")
 
 
-#here we save the data from Token_hgyulBaIApjsQZyA8qQ0yQ to the the inktest.xlsx into the  sheet Sheet1 and dont use the index
-Token_hgyulBaIApjsQZyA8qQ0yQ.to_excel("inktest.xlsx", sheet_name = "Sheet1", index = False)
+#here we save the data from Token_vBu912o4OI4zQhWoDmj75g to the the inktest.xlsx into the  sheet Sheet1 and dont use the index
+Token_vBu912o4OI4zQhWoDmj75g.to_excel("inktest.xlsx", sheet_name = "Sheet1", index = False)
 
--- src/BPMN/CombineStrategy.py
+++ src/BPMN/CombineStrategy.py
@@ -18,7 +18,7 @@ class CombineStrategy():
 class ConcatStrategy(CombineStrategy):
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        df_c = pd.concat([df_1, df_2]).drop_duplicates(keep=True)
+        df_c = pd.concat([df_1, df_2]).drop_duplicates(keep="first")
         return df_c
 
     def get_code(self, df_1: str, df_2: str) -> str:
