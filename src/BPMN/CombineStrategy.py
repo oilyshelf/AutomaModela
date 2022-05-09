9cc6f39dbdb656df0b094281ecd9775fa7c1c8a3
oilyshelf <oilyshelf@gmail.com>
2022-05-09 20:36:40 +0200

    added all combinestrats

--- src/BPMN/CombineStrategy.py
+++ src/BPMN/CombineStrategy.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import abc
-from BPMN.logger import logger
 from BPMN.TransformationStrategy import FilterStrategy
+
+# abstract base class
 
 
 class CombineStrategy():
@@ -14,43 +15,52 @@ class CombineStrategy():
     def get_code(self, df_1: str, df_2: str) -> str:
         pass
 
-
-class ConcatStrategy(CombineStrategy):
-
-    def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        df_c = pd.concat([df_1, df_2]).drop_duplicates(keep="first")
-        return df_c
-
-    def get_code(self, df_1: str, df_2: str) -> str:
-        return f"#Here we concate two Dataframes\n{df_1} = pd.concat([{df_1},{df_2}]).drop_duplicates(keep=True)\n"
+# joins
 
 
-class DiffrenceStrategy(CombineStrategy):
+class NaturaljoinStrategy(CombineStrategy):
+    def __init__(self, _index: bool = False, how: str = "inner") -> None:
+        super().__init__(self)
+        self.index = _index
+        self.how = how
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        df_c = pd.concat([df_1, df_2]).drop_duplicates(keep=False)
-        return df_c
+        if self._index:
+            return df_1.join(df_2, how=self.how)
+        return df_1.merge(df_2, how=self.how)
 
     def get_code(self, df_1: str, df_2: str) -> str:
-        return f"#Here we concate two Dataframes\n{df_1} = pd.concat([{df_1},{df_2}]).drop_duplicates(keep=False)\n"
+        if self.index:
+            return f"#join on indcies\n{df_1}.join({df_2}, how = '{self.how}')"
+        return f"#natural join on columns \n{df_1}.merge({df_2}, how = '{self.how}')"
 
 
-class CrossStrategy(CombineStrategy):
+class JoinOnStrategy(CombineStrategy):
+
+    def __init__(self, on: str, how: str = "inner") -> None:
+        super().__init__()
+        self.on = on
+        self.how = how
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        return df_1.merge(df_2, how="cross")
+        return df_1.merge(df_2, on=self.on, how=self.how)
 
     def get_code(self, df_1: str, df_2: str) -> str:
-        return f"""#Here we cross two Dataframes\n{df_1} = {df_1}.merge({df_2}, how = "cross")\n"""
+        return f"""#Here we {self.how }join on {self.on} two Dataframes\n{df_1} = {df_1}.merge({df_2}, on = "{self.on}",  how="{self.how}")\n"""
 
 
-class Intersecttrategy(CombineStrategy):
+class EquijoinStrategy(CombineStrategy):
+    def __init__(self, right: str, left: str, how: str = "inner") -> None:
+        super().__init__()
+        self.how = how
+        self.right = right
+        self.left = left
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        return df_1.merge(df_2, on=list(df_1.columns), how="inner")
+        return df_1.merge(df_2, left_on=self.left, right_on=self.right, how=self.how)
 
     def get_code(self, df_1: str, df_2: str) -> str:
-        return f"""#Here we cross two Dataframes\n{df_1} = {df_1}.merge({df_2}, on = list({df_1}.columns) , how = "inner")\n"""
+        return f"""#Here we make an equi join two Dataframes\n{df_1} = {df_1}.merge({df_2},left_on="{self.left}", right_on="{self.right}", how="{self.how}")\n"""
 
 
 class ThetaStrategy(CombineStrategy):
@@ -68,53 +78,58 @@ class ThetaStrategy(CombineStrategy):
         return f"""#Here we do a theta join on {self.query} by  crossing and then querying two Dataframes\n{df_1} = {df_1}.merge({df_2}, how = "cross")\n{self.filter.get_code(df_1)}"""
 
 
-class JoinOnStrategy(CombineStrategy):
-
-    def __init__(self, on: str, how: str = "inner") -> None:
-        super().__init__()
-        self.on = on
-        self.how = how
+# set things
+class UnionStrategy(CombineStrategy):
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        return df_1.merge(df_2, on=self.on, how=self.how)
+        df_c = pd.concat([df_1, df_2]).drop_duplicates(keep="first")
+        return df_c
 
     def get_code(self, df_1: str, df_2: str) -> str:
-        return f"""#Here we {self.how }join on {self.on} two Dataframes\n{df_1} = {df_1}.merge({df_2}, on = "{self.on}",  how="{self.how}")\n"""
+        return f"#Here we concate two Dataframes\n{df_1} = pd.concat([{df_1},{df_2}]).drop_duplicates(keep='first')\n"
 
 
-class NaturaljoinStrategy(CombineStrategy):
-    def __init__(self, _index: bool = False) -> None:
-        super().__init__(self)
-        self.index = _index
+class ConcatStrategy(CombineStrategy):
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        try:
-            if self._index:
-                return df_1.join(df_2)
-            else:
-                return df_1.merge(df_2)
-        except Exception:
-            logger.warning(f"Natural Join failed return cross product")
-            return df_1.merge(df_2, how="cross")
+        df_c = pd.concat([df_1, df_2])
+        return df_c
 
     def get_code(self, df_1: str, df_2: str) -> str:
-        return f"""#trying to do a natural join on {"index" if self._index else "columns"}
-try:
-    {df_1+"="+df_1+".join("+df_2+")" if self._index else df_1+"="+df_1+".merge("+df_2+")"}
-except Exception:
-    {df_1} = {df_1}.merge({df_2}, how = "cross")
-"""
+        return f"#Here we concate two Dataframes\n{df_1} = pd.concat([{df_1},{df_2}])\n"
 
 
-class EquijoinStrategy(CombineStrategy):
-    def __init__(self, right: str, left: str, how: str = "inner") -> None:
-        super().__init__()
-        self.how = how
-        self.right = right
-        self.left = left
+class Intersecttrategy(CombineStrategy):
 
     def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
-        return df_1.merge(df_2, left_on=self.left, right_on=self.right, how=self.how)
+        return df_1.merge(df_2, on=list(df_1.columns), how="inner")
 
     def get_code(self, df_1: str, df_2: str) -> str:
-        return f"""#Here we make an equi join two Dataframes\n{df_1} = {df_1}.merge({df_2},left_on="{self.left}", right_on="{self.right}", how="{self.how}")\n"""
+        return f"""#Here we cross two Dataframes\n{df_1} = {df_1}.merge({df_2}, on = list({df_1}.columns) , how = "inner")\n"""
+
+
+class DiffrenceStrategy(CombineStrategy):
+
+    def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
+        return pd.concat([df_1, df_2]).drop_duplicates(keep=False)
+
+    def get_code(self, df_1: str, df_2: str) -> str:
+        return f"#Here we concate two Dataframes\n{df_1} = pd.concat([{df_1},{df_2}]).drop_duplicates(keep=False)\n"
+
+
+class SubtractStrategy(CombineStrategy):
+
+    def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
+        return df_1[~df_1.apply(tuple, 1).isin(df_2.apply(tuple, 1))]
+
+    def get_code(self, df_1: str, df_2: str) -> str:
+        return f"#Here we concate two Dataframes\n{df_1} = {df_1}[~{df_1}.apply(tuple, 1).isin({df_2}.apply(tuple, 1))]\n"
+
+
+class CrossStrategy(CombineStrategy):
+
+    def combine(self, df_1: pd.DataFrame, df_2: pd.DataFrame) -> pd.DataFrame:
+        return df_1.merge(df_2, how="cross")
+
+    def get_code(self, df_1: str, df_2: str) -> str:
+        return f"""#Here we cross two Dataframes\n{df_1} = {df_1}.merge({df_2}, how = "cross")\n"""
